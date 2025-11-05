4c911cc8a15254aa5f63c5fc0a3ec6bf7357174b
Brian A <brian@dadgumsalsa.com>
2025-11-04 20:04:06 -0600

    Moving model data to separate module

---
+++ src/models/github.py
@@ -0,0 +1,28 @@
+from typing import Literal
+
+from pydantic import BaseModel
+
+
+class RepoData(BaseModel):
+    """
+    Represents a repository from GitHub.
+
+    Attributes:
+        id: The repository ID.
+        owner: The repository owner.
+        name: The repository name.
+        description: The repository description.
+        url: The repository URL.
+        visibility: Is repository public or private
+        fork: Is repository forked
+        archived: Is the repository archived
+    """
+
+    id: int
+    owner: str
+    name: str
+    description: str | None
+    url: str
+    visibility: Literal["public"] | Literal["private"]
+    fork: bool
+    archived: bool
