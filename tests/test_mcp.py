7ae4228d09dfa3446e926be18a3b7e1f2adc848d
Brian Alexander <balexander04@gmail.com>
2025-11-06 17:34:08 -0600

    Adding tests for a couple of methods

--- tests/test_mcp.py
+++ tests/test_mcp.py
@@ -1,6 +1,7 @@
 """
 Integration test for MCP server using requests
 """
+
 import pytest
 import requests
 from time import sleep
@@ -14,8 +15,30 @@ def test_mcp_server_running():
     response = requests.get("http://localhost:8001/docs")
     # Assert that the response status code is 200 (OK)
     assert (
+        response.status_code == 200
+    ), f"Expected status code 200, but got {response.status_code}"
+
+
+def test_list_repos_method_available():
+    """Checks that tool is available"""
+    response = requests.post("http://localhost:8001/github/List%20Repositories")
+    assert (
+        response.status_code == 200
+    ), f"Expected status code 200, but got {response.status_code}"
+    assert (
+        len(response.json()) > 1
+    ), f"Expected len of response.json() to be greater than 1, received: {len(response.json())}"
+
+
+def test_get_time_method_available():
+    """Checks that tool is available"""
+    response = requests.post("http://localhost:8001/time/Get%20Time")
+    assert (
             response.status_code == 200
     ), f"Expected status code 200, but got {response.status_code}"
+    assert (
+            len(response.json()) > 1
+    ), f"Expected len of response.json() to be greater than 1, received: {len(response.json())}"
 
 
 if __name__ == "__main__":
