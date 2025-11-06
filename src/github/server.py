8bfb084aec5d0f483c3587d217077e4bf7a5d087
Brian Alexander <balexander04@gmail.com>
2025-11-06 17:24:28 -0600

    Moving around modules to solve imports

--- src/github/server.py
+++ src/github/server.py
@@ -13,8 +13,8 @@ import json
 from mcp.server.fastmcp import Context, FastMCP
 from mcp.server.session import ServerSession
 
-from src.models.github import RepoData
-from src.util import make_github_request
+from models import RepoData
+from util import make_github_request
 
 
 # Create MCP instance
