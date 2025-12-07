353e0abf695d4b0ec98c1a524fd2f46349eefb61
Brian Alexander <balexander04@gmail.com>
2025-12-07 13:48:27 -0600

    Creating test class and adding setup fixture

--- tests/test_mcp.py
+++ tests/test_mcp.py
@@ -6,38 +6,60 @@ import pytest
 import requests
 from time import sleep
 
-
-def test_mcp_server_running():
-    """
-    Test that the MCP server is running by making a GET request to the docs endpoint.
-    """
-    sleep(10)  # temp measure to give time for server to load
-    response = requests.get("http://localhost:8001/docs")
-    # Assert that the response status code is 200 (OK)
-    assert (
-        response.status_code == 200
-    ), f"Expected status code 200, but got {response.status_code}"
+BASE_URL = "http://localhost:8001"
 
 
-def test_github_docs_available():
-    """Checks that tool is available"""
-    sleep(10)  # temp measure to give time for server to load
-    response = requests.get("http://localhost:8001/github/docs")
-    assert (
-        response.status_code == 200
-    ), f"Expected status code 200, but got {response.status_code}"
+def wait_for_server(wait: int = 3):
+    """Temporarily using sleep method until more advanced method is implemented"""
+    sleep(wait)
 
 
-def test_get_time_method_available():
-    """Checks that tool is available"""
-    sleep(10)  # temp measure to give time for server to load
-    response = requests.post("http://localhost:8001/time/Get%20Time")
-    assert (
-        response.status_code == 200
-    ), f"Expected status code 200, but got {response.status_code}"
-    assert (
-        len(response.json()) > 1
-    ), f"Expected len of response.json() to be greater than 1, received: {len(response.json())}"
+class TestMCP:
+
+    @pytest.fixture(scope="class", autouse=True)
+    def setup(self):
+        wait_for_server()
+
+    def test_mcp_server_running(self):
+        """
+        Test that the MCP server is running by making a GET request to the docs endpoint.
+        """
+        response = requests.get(f"{BASE_URL}/docs")
+        # Assert that the response status code is 200 (OK)
+        assert (
+            response.status_code == 200
+        ), f"Expected status code 200, but got {response.status_code}"
+
+
+    def test_github_docs_available(self):
+        """Checks that tool is available"""
+        response = requests.get(f"{BASE_URL}/github/docs")
+        assert (
+            response.status_code == 200
+        ), f"Expected status code 200, but got {response.status_code}"
+
+
+    def test_get_time_method_available(self):
+        """Checks that tool is available"""
+        timezone = {"time_zone": "America/Chicago"}
+        response = requests.post(f"{BASE_URL}/time/Get%20Time", json=timezone)
+        assert (
+            response.status_code == 200
+        ), f"Expected status code 200, but got {response.status_code}"
+        assert (
+            len(response.json()) > 1
+        ), f"Expected len of response.json() to be greater than 1, received: {len(response.json())}"
+
+
+    def test_get_time_method_available_no_timezone(self):
+        """Checks that tool is available"""
+        response = requests.post(f"{BASE_URL}/time/Get%20Time", json={})
+        assert (
+                response.status_code == 200
+        ), f"Expected status code 200, but got {response.status_code}"
+        assert (
+                len(response.json()) > 1
+        ), f"Expected len of response.json() to be greater than 1, received: {len(response.json())}"
 
 
 if __name__ == "__main__":
